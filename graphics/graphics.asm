dacb097e643ad188ab19db37ef5d2990bff399d5
Dave Tansley <dave.tansley@gmail.com>
2020-02-08 08:15:29 +0000

    Added extra frames.

--- graphics/graphics.asm
+++ graphics/graphics.asm
@@ -24,4 +24,11 @@ sprite_attrs:
     defb    066 ; 9, rock
 
 player_sprite:
-    defb    16, 56, 16, 63, 60, 56,104, 76  ; 0, default pose
+    defb   	 16, 56, 16, 63, 60, 56,104, 76 ; 0 right 1
+	defb	 16, 56, 16, 63, 60, 56,108, 64 ; 1 right 2     
+	defb	  8, 28,  8,252, 60, 28, 22, 50 ; 2 left 1
+	defb	  8, 28,  8,252, 60, 28, 54,  2 ; 3 left 2
+	defb	 64,224, 64,252,240,240,152,192 ; 4 shoot right
+	defb	 2,  7,  2, 63, 15, 15, 25,  3  ; 4 shoot left
+	defb	 16, 57,146,252, 56, 61,195,128 ; 5 up/down 1
+	defb	  8,156, 73, 63, 28,188,195,  1 ; 6 up/down 2
